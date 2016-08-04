7af2de4b8695713a969648f17fcca5e00b73cf12
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-08-04 12:42:37 +0200

    minor changes

--- www/version/index.php
+++ www/version/index.php
@@ -3,7 +3,6 @@
 	$data = [
 		"build" => getenv('CBUILDNUM'),
 		"branch" => getenv('CBRANCH'),
-		"wordpressVersion" => getenv('WORDPRESS_VERSION'),
 	];
 	header("Content-type: application/json; charset=utf-8");
 	echo json_encode($data, JSON_PRETTY_PRINT);
