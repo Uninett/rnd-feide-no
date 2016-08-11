f6265df517643cf0e40ff0eca65b6186130ed550
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-08-11 09:57:23 +0200

    update db migrate

--- bin/migratedb.sh
+++ bin/migratedb.sh
@@ -1,11 +1,4 @@
 #! /bin/bash
 
-
-
-echo "mysqldump -u ${DBUSER} --password="xxx" -h ${DBHOST} ${DBNAME}"
-echo "mysql -u ${TESTING_DBUSER} --password="xxx" -h ${TESTING_DBHOST} ${TESTING_DBNAME}"
-
-echo "mysqldump -u ${WORDPRESS_DB_USER_PROD} --password=\"${WORDPRESS_DB_PASSWORD_PROD}\" -h ${WORDPRESS_DB_HOST}  ${WORDPRESS_DB_NAME_PROD} | mysql -u ${WORDPRESS_DB_USER} --password=\"${WORDPRESS_DB_PASSWORD}\" -h ${WORDPRESS_DB_HOST} ${WORDPRESS_DB_NAME}"
-
-# mysqldump --add-drop-database -u ${DBUSER} --password="${DBPASSWORD}" -h ${DBHOST} ${DBNAME} | mysql -u ${TESTING_DBUSER} --password="${TESTING_DBPASSWORD}" -h ${TESTING_DBHOST} ${TESTING_DBNAME}
-# echo "mysqldump -u $WORDPRESS_DB_USER"
+# echo "mysqldump -u ${WORDPRESS_DB_USER_PROD} --password=\"${WORDPRESS_DB_PASSWORD_PROD}\" -h ${WORDPRESS_DB_HOST}  ${WORDPRESS_DB_NAME_PROD} | mysql -u ${WORDPRESS_DB_USER} --password=\"${WORDPRESS_DB_PASSWORD}\" -h ${WORDPRESS_DB_HOST} ${WORDPRESS_DB_NAME}"
+mysqldump -u ${WORDPRESS_DB_USER_PROD} --password="${WORDPRESS_DB_PASSWORD_PROD}" -h ${WORDPRESS_DB_HOST}  ${WORDPRESS_DB_NAME_PROD} | mysql -u ${WORDPRESS_DB_USER} --password="${WORDPRESS_DB_PASSWORD}" -h ${WORDPRESS_DB_HOST} ${WORDPRESS_DB_NAME}
